5709ab2c34016dd08686f7313bd38432a03d05b4
thsdias <thdias.84@gmail.com>
2024-06-21 15:40:54 -0300

    Exemplo 4 - Exchange DIRECT

---
+++ exemplo_04 - Exchange_DIRECT/csharp/Receiver/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+/*
+    EXCHANGE DIRECT:
+
+    Consumidor:
+
+    Declarar a Exchange: 
+        channel.ExchangeDeclare(exchange: "direct_logs", type: "direct") assegura que a exchange "direct_logs" existe.
+    Criar Fila: 
+        var queueName = channel.QueueDeclare().QueueName cria uma fila temporária com um nome gerado pelo servidor.
+    
+    Ligar Fila à Exchange: 
+        Para cada severidade passada como argumento (info, warning, error), a fila é ligada à exchange "direct_logs" com a chave de roteamento correspondente.
+    
+    Consumir Mensagens: 
+        channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer) começa a consumir mensagens da fila temporária.   
+
+    Exemplo execucao:
+        dotnet run error
+        dotnet run info warning
+*/
+
+string _exchange = "direct_logs";
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare(exchange: _exchange, 
+                            type: ExchangeType.Direct);
+
+    var queueName = channel.QueueDeclare().QueueName;
+
+    if (args.Length < 1)
+    {
+        Console.Error.WriteLine("Usage: {0} [info] [warning] [error]", Environment.GetCommandLineArgs()[0]);
+        return;
+    }
+
+    foreach (var severity in args)
+    {
+        channel.QueueBind(queue: queueName,
+                          exchange: _exchange,
+                          routingKey: severity);
+    }
+
+    Console.WriteLine(" [*] Waiting for logs. To exit press CTRL+C");
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = System.Text.Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [x] {0}:{1}", ea.RoutingKey, message);
+    };
+
+    channel.BasicConsume(queue: queueName,
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.ReadLine();
+}
---
+++ exemplo_04 - Exchange_DIRECT/csharp/Sender/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+/*
+    EXCHANGE DIRECT:
+
+    Permite que você roteie mensagens para filas com base em uma chave de roteamento (routing key) exata. 
+    Isso é útil quando você deseja que apenas determinadas filas recebam mensagens que correspondem a uma 
+    chave de roteamento específica.
+
+    Produtor:
+
+    Declare a Exchange: 
+        channel.ExchangeDeclare(exchange: "direct_logs", type: "direct") cria uma exchange do tipo direct chamada "direct_logs".
+
+    Enviar Mensagens: 
+        Para cada severidade ("info", "warning", "error"), uma mensagem é enviada para a exchange "direct_logs" com a chave de roteamento correspondente. 
+        Por exemplo, uma mensagem com a chave de roteamento "info" será enviada para a exchange "direct_logs".
+*/
+
+string _exchange = "direct_logs";
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare(exchange: _exchange, 
+                            type: "direct");
+
+    string[] messages = { "Primeiro log", "Segundo log", "Terceiro log", "Quarto log", "Quinto log" };
+    string[] severities = { "info", "error", "warning", "error", "info" };
+
+    for (int i = 0; i < 5; i++)
+    {
+        channel.BasicPublish(exchange: _exchange,
+                             routingKey: severities[i],
+                             basicProperties: null,
+                             body: Encoding.UTF8.GetBytes(messages[i]));
+        
+        Console.WriteLine($" [x] Sent {severities[i]}:{messages[i]}");
+        System.Threading.Thread.Sleep(1000);
+    }
+}
