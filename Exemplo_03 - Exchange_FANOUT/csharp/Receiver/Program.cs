073284fc67a2acdd117ad1da21677cde8c9b0fed
thsdias <thdias.84@gmail.com>
2024-06-21 15:36:09 -0300

    Exemplo 3 - Exchange FANOUT

---
+++ Exemplo_03 - Exchange_FANOUT/csharp/Receiver/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+/*
+    Consumidor:
+
+    Declare a Exchange: 
+        channel.ExchangeDeclare(exchange: "logs", type: "fanout") assegura que a exchange "logs" existe.
+    
+    Criar Fila Temporária: 
+        var queueName = channel.QueueDeclare().QueueName cria uma fila temporária com um nome gerado pelo servidor.
+    
+    Ligar Fila à Exchange: 
+        channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "") liga a fila temporária à exchange "logs".
+    
+    Consumir Mensagens: 
+        channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer) começa a consumir mensagens da fila temporária.
+*/
+
+string _exchange = "logs";
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare(_exchange, 
+                            ExchangeType.Fanout);
+
+    var queueName = channel.QueueDeclare().QueueName;
+
+    channel.QueueBind(queueName, 
+                      _exchange, 
+                      string.Empty);
+
+    Console.WriteLine(" [*] Waiting for logs. To exit press CTRL+C");
+
+    var consumer = new EventingBasicConsumer(channel);
+    
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        Console.WriteLine(" [x] {0}", Encoding.UTF8.GetString(body));
+    };
+
+    channel.BasicConsume(queueName, true, consumer);
+
+    Console.ReadLine();
+}
